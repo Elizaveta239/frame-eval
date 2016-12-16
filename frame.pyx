a1dc158336a299eff7abe3f2455429f5aaf52bd6
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2016-12-16 17:32:07 +0300

    Fix stupid mistake: frame evaluation function should return the new value. Now it shows frame's bytecode with dis.dis() function

--- frame.pyx
+++ frame.pyx
@@ -4,18 +4,13 @@ disfunc = PyObject_GetAttrString(dismod, "dis")
 def get_bytecode_for_python_code_obj(code):
     PyObject_CallFunction(<PyObject*> disfunc, "(O)", <PyObject*> code)
 
-cdef get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
-    # This function always fails with TypeError exception inside dis.py
-    _PyEval_EvalFrameDefault(frame, exc)
+cdef object get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
+    # Show bytecode for frame with dis.dis() function
     if (str(<object> frame.f_code.co_filename)).endswith("test.py"):
         # avoid infinite recursion
+        print("Show bytecode for frame:", <object> frame.f_code.co_filename, <object> frame.f_lineno)
         PyObject_CallFunction(<PyObject*> disfunc, "(O)", <PyObject*> frame.f_code)
-
-cdef show_filename_line(PyFrameObject *frame, int exc):
-    # This function work fine
-    _PyEval_EvalFrameDefault(frame, exc)
-    print(<object> frame.f_code.co_filename, <object> frame.f_lineno)
-    print('out')
+    return <object> _PyEval_EvalFrameDefault(frame, exc)
 
 def main():
     cdef PyThreadState *state = PyThreadState_Get()
