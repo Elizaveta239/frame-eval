c365c2ec80432ba4ed5d81e165fa52fd460a1ed2
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2016-12-19 14:09:52 +0300

    Use Python function call instead of C call

--- frame.pyx
+++ frame.pyx
@@ -1,15 +1,11 @@
-dismod = PyImport_ImportModule("dis")
-disfunc = PyObject_GetAttrString(dismod, "dis")
-
-def get_bytecode_for_python_code_obj(code):
-    PyObject_CallFunction(<PyObject*> disfunc, "(O)", <PyObject*> code)
+from dis import dis
 
 cdef object get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
     # Show bytecode for frame with dis.dis() function
     if (str(<object> frame.f_code.co_filename)).endswith("test.py"):
         # avoid infinite recursion
         print("Show bytecode for frame:", <object> frame.f_code.co_filename, <object> frame.f_lineno)
-        PyObject_CallFunction(<PyObject*> disfunc, "(O)", <PyObject*> frame.f_code)
+        dis(<object> frame.f_code)
     return <object> _PyEval_EvalFrameDefault(frame, exc)
 
 def main():
