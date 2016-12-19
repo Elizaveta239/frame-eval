e30a48225af2286ef6be1715a76767e1190e7b09
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2016-12-19 21:15:35 +0300

    Implement the first working version of debugger

--- frame.pyx
+++ frame.pyx
@@ -1,13 +1,17 @@
-from dis import dis
+from debugger.modify_bytecode import insert_code
+
+trace_code = None
 
 cdef object get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
     # Show bytecode for frame with dis.dis() function
     if (str(<object> frame.f_code.co_filename)).endswith("test.py"):
         # avoid infinite recursion
-        print("Show bytecode for frame:", <object> frame.f_code.co_filename, <object> frame.f_lineno)
-        dis(<object> frame.f_code)
+        new_code = insert_code(<object> frame.f_code, trace_code, 3)
+        frame.f_code = <PyCodeObject *> new_code
     return <object> _PyEval_EvalFrameDefault(frame, exc)
 
-def main():
+def main(my_trace_code):
+    global trace_code
+    trace_code = my_trace_code
     cdef PyThreadState *state = PyThreadState_Get()
     state.interp.eval_frame = get_bytecode_while_frame_eval
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import frame
+from debugger.debug_info import Debugger
 
-frame.main()
+debugger = Debugger()
 
 
+def trace():
+    debugger.call_trace()
 
-def bar():
-    return "hey"
+
+frame.main(trace.__code__)
 
 
 def foo():
-    bar()
     a = 1
     b = 2
-    return a + b
+    c = 3
+    d = 4
+    return a + b + c + d
 
 
 foo()
-
