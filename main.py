6c56a9860c97fa7156eca85d31f3aed598dcee77
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-05-16 15:23:04 +0300

    Update the main example

--- main.py
+++ main.py
@@ -4,24 +4,15 @@ import os
 filename = os.path.abspath(__file__)
 
 debugger = FrameDebugger()
-debugger.add_breakpoint(filename, 15)
-debugger.add_breakpoint(filename, 22)
+debugger.add_breakpoint(filename, 14)
 debugger.run()
-
-
-def bar():
-    t = 12
-    s = 10
-    return t + s
 
 
 def foo():
     a = 1
     b = 2
-    for i in range(3):
-        print(f"foo {i}")
-    bar()
-    return a + b
+    c = 3
+    return a + b + c
 
 
 foo()
